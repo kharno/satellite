ed1b6974d2a573d7d294fde3ffe79a77af52f1c1
bair <kharnokhoev.bv@yandex.ru>
2022-11-29 23:14:04 +0300

    add keo_to_agesk

--- runge_kutte_on_python.py
+++ runge_kutte_on_python.py
@@ -91,28 +91,59 @@ def calculate_agesk_to_keo(agesk):
     # фокальный параметр, м
     result_keo[7] = result_keo[4] * (1 - result_keo[3] * result_keo[3])
 
+    # радиус-вектор КА, м
+    result_keo[8] = result_keo[7] / (1 + result_keo[3] * math.cos(result_keo[6]))
+
     # апогейное расстояние, м
-    result_keo[8] = result_keo[7] / (1 - result_keo[3])
+    result_keo[9] = result_keo[7] / (1 - result_keo[3])
 
     # перигейное расстояние, м
-    result_keo[9] = result_keo[7] / (1 + result_keo[3])
-
-    # радиус-вектор КА, м
-    result_keo[10] = result_keo[7] / (1 + result_keo[3] * math.cos(result_keo[6]))
+    result_keo[10] = result_keo[7] / (1 + result_keo[3])
 
     return result_keo
+
+
+def calculate_keo_to_agesk(keo):
+    # Earth gravitational constant 398600.44 km^2/sec^5
+    mu = 398600.44 * 1000 * 1000 * 1000
+    agesk = [0 for item in range(6)]
+
+    if len(keo) == 6:
+        keo.append(keo[5] - keo[2])
+        keo.append(keo[4] * (1 - keo[3] * keo[3]))
+        keo.append(keo[7] / (1 + keo[3] * math.cos(keo[6])))
+
+    # нахождение радиальной и трансверсальной составляющей скорости
+    vr = math.sqrt(mu/keo[7])*keo[3]*math.sin(keo[6])
+    vt = math.sqrt(mu/keo[7])*(1+keo[3]*math.cos(keo[6]))
+
+    # нахождение скоростей в АГЭСК
+    agesk[0] = vr*(math.cos(keo[1])*math.cos(keo[5]) - math.sin(keo[1])*math.sin(keo[5])*math.cos(keo[0])) - vt*(math.cos(keo[1])*math.sin(keo[5])+math.sin(keo[1])*math.cos(keo[5])*math.cos(keo[0]))
+    agesk[1] = vr*(math.sin(keo[1])*math.cos(keo[5]) + math.cos(keo[1])*math.sin(keo[5])*math.cos(keo[0])) - vt*(math.sin(keo[1])*math.sin(keo[5])-math.cos(keo[1])*math.cos(keo[5])*math.cos(keo[0]))
+    agesk[2] = vr*math.sin(keo[5])*math.sin(keo[0]) + vt*math.cos(keo[5])*math.sin(keo[0])
+
+    # нахождение координат в АГЭСК
+    agesk[3] = keo[8] * (math.cos(keo[1])*math.cos(keo[5])-math.sin(keo[1])*math.sin(keo[5])*math.cos(keo[0]))
+    agesk[4] = keo[8] * (math.sin(keo[1])*math.cos(keo[5])+math.cos(keo[1])*math.sin(keo[5])*math.cos(keo[0]))
+    agesk[5] = keo[8] * math.sin(keo[5])*math.sin(keo[0])
+
+    return agesk
 
 
 def main():
     # example
     h = 10
-    # coordinates in AGSK for round orbit with altitude H = 500 km and zero inclination
-    prev_coord = [0, 3454.842, 6787.935, 6871000, 0, 0]
-    for iteration in range(10000):
+    # coordinates in AGESK for round orbit with altitude H = 500 km and zero inclination
+    prev_coord = calculate_keo_to_agesk([62.7*math.pi/180, 287*math.pi/180, 271*math.pi/180, 0.72, 26000000, 0])
+    print(prev_coord)
+    r = []
+    for iteration in range(8616):
+        r.append(math.sqrt(prev_coord[3]*prev_coord[3] + prev_coord[4]*prev_coord[4] + prev_coord[5]*prev_coord[5]))
         next_coord = calculate_move_runge(prev_coord, h)
         prev_coord = next_coord
 
-
+    plt.plot(r)
+    plt.show()
 
 
 if __name__ == "__main__":
